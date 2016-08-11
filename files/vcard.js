191067a432fb55c71d64f7028004cc616e19bf19
Roland Haas <rhaas@aei.mpg.de>
2016-08-11 09:54:31 -0500

    swap files for copies of AEI wordpress based one

---
+++ files/vcard.js
@@ -0,0 +1,3 @@
+
+jQuery(function($){if(typeof $.fn.colorbox!='undefined'){$('.portfolio-gallery .entry').colorbox({scalePhotos:true,scrolling:false,rel:'gal',returnFocus:false,maxWidth:'80%',maxHeight:'80%'});}
+console.log('foo');$('#main-menu li a').prepend('<em></em>');})
